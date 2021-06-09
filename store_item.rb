84e9f902f04454b738eacdc701132a02da2bd102
MEG HOWE <howem9@gmail.com>
2021-06-09 15:32:01 -0400

    tried attr_reader and attr_writer

--- store_item.rb
+++ store_item.rb
@@ -9,6 +9,9 @@ book = { color: "green", price: 27, size: "stout" }
 #puts skate[:color]
 
 class Store_items
+  attr_reader :item, :input_item, :input_color, :input_price, :input_size, :print_info
+  attr_writer :print_info
+
   def initialize(input_item, input_color, input_price, input_size)
     @item = input_item
     @color = input_color
@@ -16,30 +19,35 @@ class Store_items
     @size = input_size
   end
 
-  def color
-    @color
-  end
+  # def color
+  #   @color
+  # end
 
-  def price
-    @price
-  end
+  # def price
+  #   @price
+  # end
 
-  def size
-    @size
-  end
+  # def size
+  #   @size
+  # end
 
-  def color=(input_text)
-    @color = input_text
-  end
+  # def color=(input_text)
+  #   @color = input_text
+  # end
 
   def print_info
-    puts "The "
+    puts "The item is a very classy #{:item} "
   end
 end
 
 skate = Store_items.new("skate", "aqua", 300, 7)
 game = Store_items.new("game", "black", 60, "medium")
-puts skate.color
-puts skate.price
+bock = Store_items.new("book", "green", 27, "stout")
 
-puts "This item is a #{item.item}"
+# puts skate.color
+# puts skate.price
+# p print_info
+
+#puts "This item is a #{item.item}"
+
+skate.print_info
