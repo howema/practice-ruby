1d9682b78bb6cd0786547d3f3c648853c6597bd1
MEG HOWE <howem9@gmail.com>
2021-06-09 15:05:16 -0400

    made class out of store items

--- store_item.rb
+++ store_item.rb
@@ -6,9 +6,40 @@ book = { color: "green", price: 27, size: "stout" }
 # skate = { :color => "aqua", :price => 300, :size => 7 }
 # book = { :color => "green", :price => 27, :size => "stout" }
 
-puts skate[:color]
+#puts skate[:color]
 
-class ClassName
-  def initialize
+class Store_items
+  def initialize(input_item, input_color, input_price, input_size)
+    @item = input_item
+    @color = input_color
+    @price = input_price
+    @size = input_size
+  end
+
+  def color
+    @color
+  end
+
+  def price
+    @price
+  end
+
+  def size
+    @size
+  end
+
+  def color=(input_text)
+    @color = input_text
+  end
+
+  def print_info
+    puts "The "
   end
 end
+
+skate = Store_items.new("skate", "aqua", 300, 7)
+game = Store_items.new("game", "black", 60, "medium")
+puts skate.color
+puts skate.price
+
+puts "This item is a #{item.item}"
