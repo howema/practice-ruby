bf978af6bc44cd983808d8a41d46590da0b384b2
MEG HOWE <howem9@gmail.com>
2021-06-09 14:41:53 -0400

    store_items as a hash

---
+++ store_item.rb
@@ -0,0 +1,14 @@
+game = { color: "black", price: 60, size: "medium" }
+skate = { color: "aqua", price: 300, size: 7 }
+book = { color: "green", price: 27, size: "stout" }
+
+# game = { :color => "black", :price => 60, :size => "medium" }
+# skate = { :color => "aqua", :price => 300, :size => 7 }
+# book = { :color => "green", :price => 27, :size => "stout" }
+
+puts skate[:color]
+
+class ClassName
+  def initialize
+  end
+end
