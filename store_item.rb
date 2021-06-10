14598053c1e8455ba9c84bb7a2c9d9c64649374b
MEG HOWE <howem9@gmail.com>
2021-06-10 13:05:33 -0400

    Changed to skate shop, single hash in initialize

--- store_item.rb
+++ store_item.rb
@@ -1,53 +1,30 @@
-game = { color: "black", price: 60, size: "medium" }
-skate = { color: "aqua", price: 300, size: 7 }
-book = { color: "green", price: 27, size: "stout" }
+# skate1 = { color: "blue", price: 250, size: 10 }
+# skate2 = { color: "aqua", price: 300, size: 7 }
+# skate3 = { color: "green", price: 50, size: 9}
 
-# game = { :color => "black", :price => 60, :size => "medium" }
-# skate = { :color => "aqua", :price => 300, :size => 7 }
-# book = { :color => "green", :price => 27, :size => "stout" }
 
-#puts skate[:color]
+class Skate
+  attr_reader :input_color, :input_price, :input_size
+  attr_writer :input_color, :input_price, :input_size
 
-class Store_items
-  attr_reader :item, :input_item, :input_color, :input_price, :input_size, :print_info
-  attr_writer :print_info
-
-  def initialize(input_item, input_color, input_price, input_size)
-    @item = input_item
-    @color = input_color
-    @price = input_price
-    @size = input_size
+  def initialize(input_options)
+    @color = input_options[:color]
+    @price = input_options[:price]
+    @size = input_options[:size]
   end
-
-  # def color
-  #   @color
-  # end
-
-  # def price
-  #   @price
-  # end
-
-  # def size
-  #   @size
-  # end
-
-  # def color=(input_text)
-  #   @color = input_text
-  # end
 
   def print_info
-    puts "The item is a very classy #{:item} "
+    puts "This skate is a very classy #{@color} number with a $#{@price} price tag. It's a size #{@size}!"
   end
+
 end
 
-skate = Store_items.new("skate", "aqua", 300, 7)
-game = Store_items.new("game", "black", 60, "medium")
-bock = Store_items.new("book", "green", 27, "stout")
+skate1 = Skate.new(color: "blue", price: 250, size: 10)
+skate2 = Skate.new(color: "aqua", price: 300, size: 7)
+skate3 = Skate.new(color: "green", price: 50, size: 9)
 
-# puts skate.color
-# puts skate.price
-# p print_info
 
-#puts "This item is a #{item.item}"
+skate1.print_info
+skate2.print_info
+skate3.print_info
 
-skate.print_info
